ebb685829e46c392d1c9424f966f0fe317627d38
Muhmmad Medhat <m.medhat@zinad.net>
2025-03-17 00:58:05 +0200

    Add comments to app.component.ts for clarity on component metadata and responsibilities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// The @Component decorator is a typescript feature
+// that provides metadata
+// that determines how the component should be processed,
+// instantiated, and used at runtime.
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
+
+// The class AppComponent is the application root component
+// and is responsible for presenting the application view
 export class AppComponent {
   title = 'angular-app';
 }
