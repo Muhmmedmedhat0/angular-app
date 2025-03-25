90b4f274bd0a35bbe6c6b0f86aecacc8672a18ce
Muhmmad Medhat <m.medhat@zinad.net>
2025-03-25 16:11:24 +0200

    Refactor user component to improve image path computation and enhance user selection logging

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,4 @@
-import { Component, Input } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-
-// Generate a random index to select a random user
+import { Component, computed, Input, input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -10,13 +7,22 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
+  // The @Input decorator is a typescript feature
+  // that provides metadata
+
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
 
-  // Get the image path for the selected user
-  get imagePATH() {
-    return `/assets/users/${this.avatar}`;
-  }
+  // // The input signal decorator is a typescript feature
+  // // that provides metadata
 
-  onSelctUser() {}
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+  image = computed(() => {
+    return `/assets/users/${this.avatar}`;
+  });
+
+  onSelctUser() {
+    console.log(`${this.name} selected`);
+  }
 }
