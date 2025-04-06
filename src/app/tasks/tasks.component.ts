9360000db0d24a8fe9d37a21f20da0b8822c506c
Muhmmad Medhat <m.medhat@zinad.net>
2025-04-06 13:53:26 +0200

    Add new task component with form and backdrop; implement task addition handling

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,15 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @Component({
   selector: 'app-tasks',
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
   @Input({ required: true }) name!: string;
   @Input({ required: true }) userId!: string;
+
+  isAddingTask: boolean = false;
 
   tasks = [
     {
@@ -104,5 +107,13 @@ export class TasksComponent {
   onCompleteTask(taskId: string) {
     this.tasks = this.tasks.filter((task) => task.id !== taskId);
     console.log(`Task ${taskId} completed`);
+  }
+
+  onAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCloseTask() {
+    this.isAddingTask = false;
   }
 }
