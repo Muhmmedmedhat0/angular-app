f45470049030b3b76152ea6b96084d9599e98f4b
Muhmmad Medhat <m.medhat@zinad.net>
2025-04-07 14:58:05 +0200

    Refactor task component to remove event emitter for task completion; utilize TasksService for task removal

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,7 +1,8 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { type Task } from './task.types';
 import { CardComponent } from '../../shared/card/card.component';
 import { DatePipe } from '@angular/common';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-task',
@@ -10,10 +11,10 @@ import { DatePipe } from '@angular/common';
   styleUrl: './task.component.css',
 })
 export class TaskComponent {
+  private tasksService = inject(TasksService);
   @Input({ required: true }) task!: Task;
-  @Output() complete = new EventEmitter<string>();
 
   onCompleteTask() {
-    this.complete.emit(this.task.id);
+    this.tasksService.removeTask(this.task.id);
   }
 }
